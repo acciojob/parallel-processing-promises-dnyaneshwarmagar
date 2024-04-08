c41ab40e993160caaca4f7f5e648482d751ef164
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-08 18:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImages(imageUrls) {
+  const promises = imageUrls.map(image => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = function() {
+        resolve(img);
+      };
+      img.onerror = function() {
+        reject(new Error(`Failed to load image's URL: ${image.url}`));
+      };
+      img.src = image.url;
+    });
+  });
+
+  return Promise.all(promises);
+}
+
+document.getElementById('download-images-button').addEventListener('click', function() {
+  downloadImages(images)
+    .then(images => {
+      const outputDiv = document.getElementById('output');
+      images.forEach(img => {
+        outputDiv.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
